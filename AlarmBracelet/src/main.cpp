541ffb479df649fa274f5ebe8d89a378b4556927
aidengithub12 <147771123+aidengithub12@users.noreply.github.com>
2026-06-22 22:08:05 -0500

    First commit

---
+++ AlarmBracelet/src/main.cpp
@@ -0,0 +1,71 @@
+#include <Arduino.h>
+#include "RTClib.h"
+
+RTC_DS3231 rtc;
+RTC_DATA_ATTR uint8_t currTime[2] = {};
+RTC_DATA_ATTR uint8_t setTime[2] = {};
+// bool alarmReached = false;
+RTC_DATA_ATTR bool hasInit = false;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  
+  pinMode(23, OUTPUT); //Change pin number to the one connected to the vibration motor
+
+  while (!Serial) {
+    delay(1); // wait for serial port to connect. Needed for native USB
+  }
+  Serial.println("Serial is initialized...");
+  
+  
+  while (!rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    while (1) delay(10);
+  }
+  if (!hasInit) {
+    currTime[0] = rtc.now().hour();
+    currTime[1] = rtc.now().minute();
+    setTime[0] = rtc.now().hour();
+    setTime[1] = rtc.now().minute() + 1;
+    hasInit = true;
+  }
+  currTime[0] = rtc.now().hour();
+  currTime[1] = rtc.now().minute();
+
+  if (rtc.lostPower()) {
+    Serial.println("RTC lost power, let's set the time!");
+    // When time needs to be set on a new device, or after a power loss, the
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+    
+  }
+
+  Serial.println("RTC is initialized...");
+  if ((Serial.available() > 0) && !hasInit) {
+    Serial.print("What time should you wake? (HH:MM:(A/P)\\:");
+    String timestr = Serial.readStringUntil('\\');
+    setTime[0] = timestr.substring(0, 2).toInt();
+    setTime[1] = timestr.substring(3, 5).toInt();
+  }
+
+  if (currTime[0] >= setTime[0] && currTime[1] >= setTime[1]) {
+    Serial.println("Alarm reached!");
+    delay(1000000  );
+    digitalWrite(23, HIGH); // Turn on vibration motor
+    delay(1000000);
+    digitalWrite(23, LOW); // Turn off vibration motor
+  }
+  Serial.println("Checking alarm...");
+  esp_sleep_enable_timer_wakeup(1800 * 1000000ULL);
+  delay(1000);
+  Serial.flush(); 
+  esp_deep_sleep_start();
+}
+
+void loop() {;}
+
