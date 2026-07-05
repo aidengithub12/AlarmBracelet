0f40b1814fca5deae0edfc1c4e4f004e92e81e45
aidengithub12 <147771123+aidengithub12@users.noreply.github.com>
2026-07-05 01:54:55 -0500

    improved alarm setoff logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <Arduino.h>
 #include "RTClib.h"
 
+
+#define setHour 0
+#define setMinute 0
+#define deltaTime 10 //minutes
+
 RTC_DS3231 rtc;
 RTC_DATA_ATTR uint8_t currTime[2] = {};
 RTC_DATA_ATTR uint8_t setTime[2] = {};
@@ -17,8 +22,12 @@ void setup() {
     delay(1); // wait for serial port to connect. Needed for native USB
   }
   Serial.println("Serial is initialized...");
-  
-  
+
+  // //motor test
+  // digitalWrite(23, HIGH); // Turn on vibration motor
+  // delay(1000);
+  // digitalWrite(23, LOW); // Turn off vibration motor
+
   while (!rtc.begin()) {
     Serial.println("Couldn't find RTC");
     Serial.flush();
@@ -27,8 +36,8 @@ void setup() {
   if (!hasInit) {
     currTime[0] = rtc.now().hour();
     currTime[1] = rtc.now().minute();
-    setTime[0] = rtc.now().hour();
-    setTime[1] = rtc.now().minute() + 1;
+    setTime[0] = setHour;
+    setTime[1] = setMinute;
     hasInit = true;
   }
   currTime[0] = rtc.now().hour();
@@ -37,7 +46,7 @@ void setup() {
   if (rtc.lostPower()) {
     Serial.println("RTC lost power, let's set the time!");
     // When time needs to be set on a new device, or after a power loss, the
-    // following line sets the RTC to the date & time this sketch was compiled
+// following line sets the RTC to the date & time this sketch was compiled 
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     // This line sets the RTC with an explicit date & time, for example to set
     // January 21, 2014 at 3am you would call:
@@ -53,15 +62,16 @@ void setup() {
     setTime[1] = timestr.substring(3, 5).toInt();
   }
 
-  if (currTime[0] >= setTime[0] && currTime[1] >= setTime[1]) {
+  if (((currTime[0] >= setTime[0]) && (currTime[0] <= setTime[0] + deltaTime / 60)) && ((currTime[1] >= setTime[1]) && (currTime[1] <= setTime[1] + deltaTime))) {
     Serial.println("Alarm reached!");
-    delay(1000000  );
+    delay(1000);
     digitalWrite(23, HIGH); // Turn on vibration motor
-    delay(1000000);
+    delay(1000);
     digitalWrite(23, LOW); // Turn off vibration motor
+    hasInit = false;
   }
   Serial.println("Checking alarm...");
-  esp_sleep_enable_timer_wakeup(1800 * 1000000ULL);
+  esp_sleep_enable_timer_wakeup(deltaTime * 60 * 1000000ULL);
   delay(1000);
   Serial.flush(); 
   esp_deep_sleep_start();
